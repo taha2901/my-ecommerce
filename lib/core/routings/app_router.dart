b465bb2bb6f303c33a7032341f632b522b48856c
Taha Hamada <93037873+taha2901@users.noreply.github.com>
2025-08-03 15:13:35 +0300

    remove address page and logic

--- lib/core/routings/app_router.dart
+++ lib/core/routings/app_router.dart
@@ -1,7 +1,5 @@
 import 'package:ecommerce_app/core/routings/routers.dart';
 import 'package:ecommerce_app/core/widget/cutome_bottom_nav_bar.dart';
-import 'package:ecommerce_app/features/address/logic/cubit/address_cubit.dart';
-import 'package:ecommerce_app/features/address/ui/choose_location_page.dart';
 import 'package:ecommerce_app/features/auth/ui/register_page.dart';
 import 'package:ecommerce_app/features/cart/ui/cart_page.dart';
 import 'package:ecommerce_app/features/home/logic/products_details_cubit/product_details_cubit.dart';
@@ -32,46 +30,6 @@ class AppRouter {
         return MaterialPageRoute(
           builder: (_) => const RegisterPage(),
         );
-
-      // case Routers.adminDashboardRoute:
-      //   return MaterialPageRoute(
-      //     builder: (context) => AdminGuard(
-      //       authCubit: context.read<AuthCubit>(),
-      //       child: const AdminDashboardScreen(),
-      //     ),
-      //   );
-
-      // case Routers.adminProductsRoute:
-      //   return MaterialPageRoute(
-      //     builder: (context) => AdminGuard(
-      //       authCubit: context.read<AuthCubit>(),
-      //       child: const AdminProductsScreen(),
-      //     ),
-      //   );
-
-
-
-      case Routers.chooseLocation:
-        return MaterialPageRoute(
-          builder: (_) => BlocProvider(
-            create: (context) {
-              final cubit = ChooseLocationCubit();
-              cubit.fetchLocations();
-              return cubit;
-            },
-            child: const ChooseLocationPage(),
-          ),
-          settings: settings,
-        );
-      // case Routers.addNewCardRoute:
-      //   final paymentCubit = settings.arguments as PaymentCubit;
-      //   return MaterialPageRoute(
-      //     builder: (_) => BlocProvider.value(
-      //       value: paymentCubit,
-      //       child: const AddNewCardPage(),
-      //     ),
-      //     settings: settings,
-      //   );
 
       case Routers.productDetailsRoute:
         final String productId = settings.arguments as String;
